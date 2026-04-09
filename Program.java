c3c10d55c229f22a1fd3d68a4d75ee4c6791b289
mohit-8516 <mohitshrivastav429@gmail.com>
2026-04-09 12:50:16 +0530

    Program

--- Program.java
+++ Program.java
@@ -23,3 +23,45 @@
 ////////////////////////////////////////////////////
 //Factorial number
 
+// import java.util.Scanner;
+
+// public class Program{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter a number: ");
+//         int num = sc.nextInt();
+//         long fact = 1;
+
+//         for (int i = 1; i <= num; i++) {
+//             fact *= i;   // multiply fact by i
+//         }
+
+//         System.out.println("Factorial of " + num + " is: " + fact);
+//         sc.close();
+//     }
+// }
+
+
+//////////////////////////////////////////
+/// Palindrome number
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your word: ");
+        String str = sc.nextLine();
+
+        String reversed = new StringBuilder(str).reverse().toString();
+
+        if (str.equalsIgnoreCase(reversed)) {
+            System.out.println(str + " is a Palindrome");
+        } else {
+            System.out.println(str + " is not a Palindrome");
+        }
+
+        sc.close();
+    }
+}
