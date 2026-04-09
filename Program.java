bb23d5ec915d11f83d0080e00244e932d027d6cb
mohit-8516 <mohitshrivastav429@gmail.com>
2026-04-09 12:33:32 +0530

    Program

---
+++ Program.java
@@ -0,0 +1,25 @@
+
+// //Odd and Even
+// import java.util.Scanner;
+
+// public class Program {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter a number: ");
+//         int num = sc.nextInt();
+
+//         if (num % 2 == 0) {
+//             System.out.println(num + "  is Even Number");
+//         } else {
+//             System.out.println(num + " is Odd Number");
+//         }
+
+//         sc.close();
+//     }
+// }
+
+
+////////////////////////////////////////////////////
+//Factorial number
+
