0a0a5a698a06ea82ce968fb9645428a6cf929731
mohit-8516 <mohitshrivastav429@gmail.com>
2026-04-09 14:01:19 +0530

    Program

--- Program.java
+++ Program.java
@@ -44,22 +44,47 @@
 
 
 //////////////////////////////////////////
-/// Palindrome number
-import java.util.Scanner;
+// / Palindrome number
+// import java.util.Scanner;
+
+// public class Program {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter your word: ");
+//         String str = sc.nextLine();
+
+//         String reversed = new StringBuilder(str).reverse().toString();
+
+//         if (str.equalsIgnoreCase(reversed)) {
+//             System.out.println(str + " is a Palindrome");
+//         } else {
+//             System.out.println(str + " is not a Palindrome");
+//         }
+
+//         sc.close();
+//     }
+// }
+
+///////////////////////////////////////////////////////////////
+/// fibonacci series
+ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter your word: ");
-        String str = sc.nextLine();
+        System.out.print("Enter how many terms: ");
+        int n = sc.nextInt();
 
-        String reversed = new StringBuilder(str).reverse().toString();
+        int a = 0, b = 1;
+        System.out.print("Fibonacci Series: ");
 
-        if (str.equalsIgnoreCase(reversed)) {
-            System.out.println(str + " is a Palindrome");
-        } else {
-            System.out.println(str + " is not a Palindrome");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
         }
 
         sc.close();
