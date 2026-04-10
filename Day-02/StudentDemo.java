b1d18e90801d84d5475988a4e8a230261d434425
mohit-8516 <mohitshrivastav429@gmail.com>
2026-04-10 12:46:42 +0530

    student

---
+++ Day-02/StudentDemo.java
@@ -0,0 +1,28 @@
+class Student {
+    // Attributes 
+    String name;
+    int rollNo;
+    int age;
+    String course;
+//creating a method to display student details
+    void displayDetails() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Roll Number: " + rollNo);
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+    }
+}
+
+public class StudentDemo {
+    public static void main(String[] args) {
+      
+        Student s1 = new Student();
+
+        s1.name = "Rahul";
+        s1.rollNo = 101;
+        s1.age = 20;
+        s1.course = "Computer Science";
+
+        s1.displayDetails();
+    }
+}
