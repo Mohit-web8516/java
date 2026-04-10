d620b32c42bc92622e730f74ebddd9b3d09a9c44
mohit-8516 <mohitshrivastav429@gmail.com>
2026-04-10 11:44:08 +0530

    Student

--- Day-02/StudentMarks.java
+++ Day-02/StudentMarks.java
@@ -1,10 +1,10 @@
-
-public class StudentMarks {
-    public static void main(String[] args) {
-        int marks[] ={ 85,90,78,88,95};
-        // System.out.println("Marks of 5 students:");
-        for(int i = 0; i < marks.length; i++) {
-            System.out.println("Student " + (i+1) + " Marks is =  " + marks[i]);
-        }
-    }
-}
+//Create an array to store marks of 5 students and them using loop.
+// public class StudentMarks {
+//     public static void main(String[] args) {
+//         int marks[] ={ 85,90,78,88,95,55,44,33,77,89};
+//         // System.out.println("Marks of 5 students:");
+//         for(int i = 0; i < marks.length; i++) {
+//             System.out.println("Student " + (i+1) + " Marks is = " + marks[i]);
+//         }
+//     }
+// }
