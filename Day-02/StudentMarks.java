6183fccbf892290eeb1a5c48c32b878d88a44d3b
mohit-8516 <mohitshrivastav429@gmail.com>
2026-04-10 11:12:16 +0530

    Student

---
+++ Day-02/StudentMarks.java
@@ -0,0 +1,10 @@
+
+public class StudentMarks {
+    public static void main(String[] args) {
+        int marks[] ={ 85,90,78,88,95};
+        // System.out.println("Marks of 5 students:");
+        for(int i = 0; i < marks.length; i++) {
+            System.out.println("Student " + (i+1) + " Marks is =  " + marks[i]);
+        }
+    }
+}
