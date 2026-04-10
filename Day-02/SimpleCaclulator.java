4e5a12711a8d5b59280a3fe63640b5942559c5eb
mohit-8516 <mohitshrivastav429@gmail.com>
2026-04-10 12:45:29 +0530

    CALCULATOR

---
+++ Day-02/SimpleCaclulator.java
@@ -0,0 +1,12 @@
+public class SimpleC
+
+
+
+
+
+
+
+System.out.println("Addition : " +add(num1+num2));
+System.out.println("Subtraction : " +add(num1+num2));
+System.out.println("Multiplication : " +add(num1+num2));
+System.out.println("Division : " +add(num1+num2));
