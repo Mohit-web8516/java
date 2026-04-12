a4b859ffd10dd2e92a2de389c9f4d797e135e5ce
mohit-8516 <mohitshrivastav429@gmail.com>
2026-04-12 14:10:53 +0530

    ProductBiling

--- Core-Java-Oop-Assignment/ProductBillingQ4/ProductBillingDemo.java
+++ Core-Java-Oop-Assignment/ProductBillingQ4/ProductBillingDemo.java
@@ -21,11 +21,15 @@ class ProductBilling {
 public class ProductBillingDemo {
     public static void main(String[] args) {
         ProductBilling billing = new ProductBilling();
-
+        // assigning singal values
         double bill1 = billing.calculateBill(100.50);
+        //assigning Two values 
         double bill2 = billing.calculateBill(100.50, 200.75);
+        //assigning three values 
         double bill3 = billing.calculateBill(100.50, 200.75, 150.25);
+        
 
+        //DISPLAY ALL THE BILL AS FINAL OUTPUT
         System.out.println("Bill of  1 product: " + bill1);
         System.out.println("Bill of 2 products: " + bill2);
         System.out.println("Bill of 3 products: " + bill3);
