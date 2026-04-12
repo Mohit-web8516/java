15bf855e7022e5733305a34979cdab09c20aee35
mohit-8516 <mohitshrivastav429@gmail.com>
2026-04-12 14:12:01 +0530

    StudentMarks

--- Core-Java-Oop-Assignment/StudentMarksQ3/StudentMarks.java
+++ Core-Java-Oop-Assignment/StudentMarksQ3/StudentMarks.java
@@ -1,16 +1,16 @@
-
+/*As an admin, I want to store marks of 5 students so that I can display them one by one. */
 
 public class StudentMarks {
 
     public static void main(String[] args) {
-        // Step 1: Create an array of 5 marks
+        // Create an array of 5 marks
         int[] marks = {34,56,78,99,78};
-
-        
-       
-        // Step 3: Print marks one by one using for loop
+  
         System.out.println("Marks of 5 students:");
+        //use loop for printing all the marks
         for (int i = 0; i < marks.length; i++) {
+
+            //DISPLAY THE FINAL OUTPUT
             System.out.println("Marks of Student  " + (i+1) + " is : " + marks[i]);
         }
     }
