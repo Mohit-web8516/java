7a28fc746dbe5d87af51089ee39cdbeb97a7f09d
mohit-8516 <mohitshrivastav429@gmail.com>
2026-04-13 13:50:48 +0530

    Serialization

---
+++ Day-04/Serialization.java
@@ -0,0 +1,44 @@
+
+//write a program to saving an object to a file using  serialization..
+
+//student class must implement serializeable
+
+// import java.io.FileOutputStream;
+// import java.io.IOException;
+// import java.io.ObjectOutputStream;
+// import java.io.Serializable;
+
+// class Student implements Serializable {
+//     int id;
+//     String name;
+
+//     Student(int id, String name) {
+//         this.id = id;
+//         this.name = name;
+
+//     }
+
+//     void Display() {
+//         System.out.println("Id: " + id);
+//         System.out.println("Name: " + name);
+//     }
+// }
+
+// public class Serialization {
+//     public static void main(String[] args) {
+//         try{
+//             Student s1 = new Student(101, "komal");
+
+//             FileOutputStream fos = new FileOutputStream("Student.ser");
+
+//             ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+//             oos.writeObject(s1);
+//             oos.close();
+//             fos.close();
+//             System.out.println("Object serialized Successfully!!!!!!");
+//         }catch (IOException e){
+//             System.out.println("Error: " + e.getMessage());
+//         }
+//     }
+// }
