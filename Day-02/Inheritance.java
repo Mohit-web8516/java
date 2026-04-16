24e015b248c6341999c81a97e16e3fbb33e66b1b
mohit-8516 <mohitshrivastav429@gmail.com>
2026-04-16 11:16:14 +0530

    Changed

--- Day-02/Inheritance.java
+++ Day-02/Inheritance.java
@@ -1,41 +1,62 @@
 
-    // Parent class
-class Person {
-    String name ;
-    int age ;
+//     // Parent class
+// class Person {
+//     String name ;
+//     int age ;
 
-    void displayPerson() {
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
+//     void displayPerson() {
+//         System.out.println("Name: " + name);
+//         System.out.println("Age: " + age);
+//     }
+// }
+
+// // Child class inheriting from Person
+// class Student extends Person {
+//     int rollNo;
+//     String course;
+
+//     void displayStudent() {
+//         // Reuse parent method
+//         displayPerson();
+//         System.out.println("Roll Number: " + rollNo);
+//         System.out.println("Course: " + course);
+//     }
+// }
+
+// public class Inheritance {
+//     public static void main(String[] args) {
+//         // Create child object
+//         Student s1 = new Student();
+
+//         // Assign values 
+//         s1.name = "Yash";
+//         s1.age = 20;
+//         s1.rollNo = 101;
+//         s1.course = "Computer Science";
+
+//         // Display details
+//         s1.displayStudent();
+//     }
+// }
+
+////////////////////////////////////////////////////////
+//single inheritance in java
+class Parent {
+    void parentMethod() {
+        System.out.println("This is the parent method.");
     }
 }
 
-// Child class inheriting from Person
-class Student extends Person {
-    int rollNo;
-    String course;
-
-    void displayStudent() {
-        // Reuse parent method
-        displayPerson();
-        System.out.println("Roll Number: " + rollNo);
-        System.out.println("Course: " + course);
+class Child extends Parent {
+    void childMethod() {
+        System.out.println("This is the child method.");
     }
 }
 
 public class Inheritance {
     public static void main(String[] args) {
-        // Create child object
-        Student s1 = new Student();
-
-        // Assign values 
-        s1.name = "Yash";
-        s1.age = 20;
-        s1.rollNo = 101;
-        s1.course = "Computer Science";
-
-        // Display details
-        s1.displayStudent();
+        Child c1 = new Child();
+        c1.parentMethod(); // Inherited from Parent
+        c1.childMethod();  // Defined in Child
     }
 }
-
