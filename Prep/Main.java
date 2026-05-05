f0004f7e7f9bfe2123da19d451a8ab55dd986864
mohit-8516 <mohitshrivastav429@gmail.com>
2026-05-05 12:45:19 +0530

    Main

---
+++ Prep/Main.java
@@ -0,0 +1,90 @@
+// public class variable {
+//     public static void main(String[] args) {
+//         int a = 23;
+//         int b = 45;
+//         int c = 46;
+//         if(a > b && a>c ){
+//             System.out.println("dolllyyyyyyy");
+//         }
+//         else if (b > a && b > c) {
+//             System.out.println("MAY BE !!!");
+//         } else {
+            
+//             System.out.println(c + " hamba damba khamba!!!!!!!!");
+//         }
+//     }
+// }
+
+/////////////////////////////////////
+// public class variable{
+//     public static void main(String[] args) {
+        
+//         int n=1;
+//         do
+//         {
+//         System.out.println(n);
+//         n++;
+//         } while(n<=10);
+//     }
+// }
+
+//////////////////////////////////////
+// class Student {
+//     int id;
+//     String name;
+//     String clg;
+
+//     void display() {
+//         System.out.println(id);
+//         System.out.println(name);
+//         System.out.println(clg);
+//     }
+// }
+// public class Main {
+//     public static void main(String[] args) {
+
+//         Student s1 = new Student();
+//         Student s2 = new Student();
+        
+
+//         s1.id = 1;
+//         s1.name = "Rahul";
+//         s1.clg = "gn";
+        
+//         s2.id = 2;
+//         s2.name = "Amit";
+//         s2.clg = "gnc";
+
+//         s1.display();
+//         s2.display();
+//     }
+// }
+//////////////////////////////////////////
+// class Student {
+//     int id;
+//     String name;
+
+//     void display() {
+//         System.out.println("ID: " + id);
+//         System.out.println("Name: " + name);
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+
+//         // Creating object
+//         Student s1 = new Student();
+//         Student s2 = new Student();
+
+//         // Assigning values
+//         s1.id = 101;
+//         s1.name = "Mohit";
+//         s2.id = 102;
+//         s2.name = "Rohit";
+
+//         // Calling method
+//         s1.display();
+//         s2.display();
+//     }
+// }
