302924e297e0187758768b49060a6a3ae3ac14e0
mohit-8516 <mohitshrivastav429@gmail.com>
2026-05-05 12:16:00 +0530

    variable

---
+++ Prep/variables.java
@@ -0,0 +1,32 @@
+// public class variable {
+//     public static void main(String[] args) {
+//         int a = 23;
+//         int b = 45;
+//         int c = 46;
+//         if(a > b && a>c ){
+//             System.out.println("dolllyyyyyyy");
+//         }
+//         else if (b > a && b > c) {
+//             System.out.println("MAY BE !!!");
+//         } else {
+            
+//             System.out.println(c + " hamba damba khamba!!!!!!!!");
+//         }
+//     }
+// }
+
+/////////////////////////////////////
+// public class variable{
+//     public static void main(String[] args) {
+        
+//         int n=1;
+//         do
+//         {
+//         System.out.println(n);
+//         n++;
+//         } while(n<=10);
+//     }
+// }
+
+//////////////////////////////////////
+// public 
