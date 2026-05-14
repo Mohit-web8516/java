0623f43d3cf2bdc1d0938f8ee44d4f08f87eef36
mohit-8516 <mohitshrivastav429@gmail.com>
2026-05-14 08:18:52 +0530

    break

--- Prep/book.java
+++ Prep/book.java
@@ -208,3 +208,17 @@
 //         }
 //     }
 // }
+
+// class book {
+
+//     public static void main(String args[]) {
+
+//         for(int i = 1; i <= 5; i++) {
+
+//             if(i == 3)
+//                 continue;
+
+//             System.out.println(i);
+//         }
+//     }
+// }
